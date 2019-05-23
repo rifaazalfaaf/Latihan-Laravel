b8c2d7305d293ca4045a72aff623ee4e5c1895d5
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2019-05-23 23:44:03 +0700

    Tutorial 8 - Sistem Template Blade Laravel

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Route::get('/pegawai/{nama}','PegawaiController@index');
 Route::get('/formulir', 'PegawaiController@formulir'); //menjalankan method formulir
 
 Route::post('/formulir/proses','PegawaiController@proses'); //menjalankan methode proses
+
+Route::get('/blog', 'BlogController@home');
+
+Route::get('/blog/tentang', 'BlogController@tentang');
+
+Route::get('/blog/kontak', 'BlogController@kontak');
